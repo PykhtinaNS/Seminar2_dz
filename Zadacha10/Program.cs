6db58b64550de5ada3bc8cc1db7106275b94ccaa
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-31 18:14:30 +0300

    zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int count = chislo.ToString().Length;
+if (count < 3 || count > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine("Вторая цифра числа: ");
+    Console.WriteLine(Rezult(chislo));
+}
+int Rezult(int ch)
+{
+    int cifra = ((ch / 10) % 10);
+    return cifra;
+}
+
